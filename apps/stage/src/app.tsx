c621c0bfbe4697229045a138ee81cc5a05895c71
gavinssr <gavin1740@gmail.com>
2026-04-20 11:15:43 +0800

    创建第一次构建表单组件的专用分支，开始工作前修复了红头文件和启动服务时的终端预警修复

--- apps/stage/src/app.tsx
+++ apps/stage/src/app.tsx
@@ -4,6 +4,6 @@ import '../../../packages/tokens/src/web-vars.css';
 import './app.css';
 import './shell/styles/layout.module.css';
 
-export default function App(props) {
+export default function App(props: React.PropsWithChildren) {
   return props.children;
 }
